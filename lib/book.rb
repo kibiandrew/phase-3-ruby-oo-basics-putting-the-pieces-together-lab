dcbed7a74284f799240c6e8ee277be25295716f8
Andrew Kibiwott <kibiandrew@gmail.com>
2023-02-24 15:03:25 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+    def initialize(title)
+        @title=title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
+end
+new_book=Book.new("Riverside")
+puts new_book.title
 
+new_book.author="madder"
+puts new_book.author
+
+new_book.page_count=1222
+puts new_book.page_count
+
+new_book.genre="madderesss"
+puts new_book.genre
+
+new_book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand=brand
+    end
+    def cobble
+        @condition="new"
+        puts "Your shoe is as good as new!"
+    end
+end
+showty=Shoe.new("Nike")
+puts showty.brand
+
+showty.color="blue"
+showty.color
+
+showty.size=31
+showty.size
+
+showty.material="cotton"
+showty.material
+
+showty.condition="new"
+showty.condition
+
+showty.cobble
+puts showty.condition 
